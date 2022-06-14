0aececf86cd4d877ec85fc8e8422141b83720256
jesse <jesse.j.lamsa@gmail.com>
2022-06-14 20:05:06 +0300

    asd

--- javascript.js
+++ javascript.js
@@ -3,16 +3,17 @@ const openLibraryBtn = document.querySelector(".open-library");
 const library = document.querySelector(".library")
 const addBtn = document.querySelector(".addBtn");
 const form = document.querySelector("form");
+const deleteConfirmForm = document.querySelector(".delete-confirm")
 
 let myLibrary = [];
 
-function Book (title, author, read) {
+function Book(title, author, read) {
     this.title = title;
     this.author = author;
     this.read = read;
 }
 
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
     if (this.read == "READ") this.read = "NOT YET READ"
     else this.read = "READ";
     displayLibrary();
@@ -37,47 +38,67 @@ function addBookToLibrary() {
 
 
 function displayLibrary() {
+   
+    if (this.textContent == "OPEN LIBRARY") library.classList.toggle("library-hidden");
 
-    if (library.classList.contains("library-hidden")) library.classList.toggle("library-hidden");
+    if ((this.textContent != "OPEN LIBRARY") && library.classList.contains("library-hidden")) library.classList.toggle("library-hidden");
 
     while (library.lastChild) library.removeChild(library.lastChild);
 
-    for (i = 0; i < myLibrary.length; i++){
+    for (i = 0; i < myLibrary.length; i++) {
+        //Tän kaiken vois tehä omassa aliohjelmassaan
+        //createBookCard(myLibrary[i])
+
         let newBookCard = document.createElement('div');
         newBookCard.classList.add('book-card');
         newBookCard.setAttribute('id', i);
-        newBookCard.textContent += `TITLE: ${myLibrary[i].title} \n \n`;
-        newBookCard.textContent += `AUTHOR: ${myLibrary[i].author} \n \n`;
+        newBookCard.textContent += `TITLE:  ${myLibrary[i].title} \n \n`;
+        newBookCard.textContent += `AUTHOR:  ${myLibrary[i].author} \n \n`;
         newBookCard.textContent += `${myLibrary[i].read} \n \n`;
 
-        let toggleReadBtn = newBookCard.appendChild(document.createElement('button'));
+        let cardButtonContainer = newBookCard.appendChild(document.createElement('div'));
+
+        let toggleReadBtn = cardButtonContainer.appendChild(document.createElement('button'));
         toggleReadBtn.textContent = 'TOGGLE READ';
         toggleReadBtn.addEventListener("click", toggleReadLaunch);
 
-
-        let removeBtn = newBookCard.appendChild(document.createElement('button'));
+        let removeBtn = cardButtonContainer.appendChild(document.createElement('button'));
         removeBtn.textContent = 'REMOVE BOOK';
         removeBtn.addEventListener("click", removeBook);
 
         library.appendChild(newBookCard);
     }
+
+    if (myLibrary.length == 0) {
+        while (library.lastChild) library.removeChild(library.lastChild);
+        let messageLibraryEmpty = document.createElement('p');
+        messageLibraryEmpty.textContent = "Library is empty, add a book!";
+        library.appendChild(messageLibraryEmpty);
+    }
 }
 
+/*
+function createBookCard (book) {
+    const newBookCard = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const buttonConainer = document.createElement('div');
+    const removeButton = document.createElement('button');
+    const toggleReadBtn = document.createElement('button');
+}
+*/
+
 function toggleReadLaunch() {
-    myLibrary[this.parentNode.getAttribute('id')].toggleRead();
+    myLibrary[this.parentNode.parentNode.getAttribute('id')].toggleRead();
 }
 
 function removeBook() {
-    myLibrary.splice(this.parentNode.getAttribute('id'), 1);
+
+    myLibrary.splice(this.parentNode.parentNode.getAttribute('id'), 1);
     displayLibrary();
 }
 
 
-
-
-
 addBtn.addEventListener("click", addBookToLibrary);
-
-openLibraryBtn.addEventListener("click", () => { library.classList.toggle("library-hidden") });
-
+openLibraryBtn.addEventListener("click", displayLibrary);
 openFormBtn.addEventListener("click", () => { form.classList.toggle("form-hidden") });
